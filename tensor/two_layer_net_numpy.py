Fix "gradient of x wrt to loss".

tensor/two_layer_net_numpy.py
@@ -35,7 +35,7 @@ for t in range(500):
   loss = np.square(y_pred - y).sum()
   print(t, loss)
   
-  # Backprop to compute gradients of w1 and w2 with respect to loss
+  # Backprop to compute gradients of loss with respect to w1 and w2
   grad_y_pred = 2.0 * (y_pred - y)
   grad_w2 = h_relu.T.dot(grad_y_pred)
   grad_h_relu = grad_y_pred.dot(w2.T)